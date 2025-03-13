7c92b2af76a81b62ee1e07b72d292457e0acfb7d
theo <haris444.hv@gmail.com>
2025-03-13 17:35:18 +0200

    Fix API Url

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth';
+const API_URL = 'https://habit-tracker-backend-0576.onrender.com';
 
 export interface User {
     id: number;
